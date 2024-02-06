88b9faa22fdcad1ce293384b5d46090fb6160d55
Cedric O <ocedric2012@gmail.com>
2024-02-06 10:26:46 +0100

    watch list models created

---
+++ app/models/movie.rb
@@ -0,0 +1,15 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  validates :title, uniqueness: true
+  validates :content, presence: true
+  before_destroy :check_for_bookmarks
+
+  private
+
+  def check_for_bookmarks
+    if bookmarks.any?
+      errors.add(:base, "cannot delete movie because it is referenced in at least one bookmark")
+      throw :abort
+    end
+  end
+end
